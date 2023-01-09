986689cece494fe9f9d54b913dfd06f62811f65c
brunocriacoes <br.rafael@outlook.com>
2023-01-09 12:06:09 -0300

    css editor e animação single page

--- pages/project/[slug].tsx
+++ pages/project/[slug].tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
 import HeaderComponents from "../../components/HeaderComponents";
 import FooterComponents from "../../components/FooterComponents";
 import Image from 'next/image'
 import Language from "../../components/Language";
 
 export default function ProjectBySlug({ allPosts, lang }: any) {
+	const [visible, setVisible] = useState(1)
+
+
+	useEffect(() => {
+		if (document) {
+			document.addEventListener("scroll", scrollShow)
+		}
+	}, [])
+
+	function scrollShow() {
+		const offsetTop = document.documentElement.scrollTop || document.body.scrollTop || 0
+		if (offsetTop > 70) {
+			setVisible(0)
+		} else {
+			setVisible(1)
+		}
+	}
+
+
 	return (
 		<div>
 			<HeaderComponents lang={lang} />
+			<div className="block w-full h-[100vh]"></div>
 			{allPosts.map((p: any) => (
 				<div key={p.id}>
-					<div className="fixed top-0 left-0 w-[100vw] z-[-1] h-[100vh]">
+					<div className={"transition-all duration-300 fixed top-0 left-0 w-[100vw] z-[-1] h-[100vh] " + (visible ? 'opacity-[1]' : 'opacity-[0]')}>
 						<Image
 							alt={p.title}
 							src={p.image_full}
@@ -18,9 +38,12 @@ export default function ProjectBySlug({ allPosts, lang }: any) {
 							objectFit="cover"
 						/>
 					</div>
+					<div className="container">
 
-					<h1 className="text-white">{p.title}</h1>
-					<div dangerouslySetInnerHTML={{ __html: p.content }} />
+						<h1 className="text-white text-[70px] font-hk font-bold">{p.title}</h1>
+						<div dangerouslySetInnerHTML={{ __html: p.content }} />
+					</div>
+
 				</div>
 			))}
 			<FooterComponents />
@@ -28,16 +51,7 @@ export default function ProjectBySlug({ allPosts, lang }: any) {
 	);
 }
 
-type paths = {
-	slug?: string
-}
-
 export async function getStaticPaths() {
-	// let products: paths[];
-	// products = []
-	// const paths = products.map(category => {
-	// 	return { params: { slug: category.slug } }
-	// })
 	return {
 		paths: [],
 		fallback: 'blocking'
