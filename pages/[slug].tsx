b18b550b67437097fae26bf26d9b0e3ef6d41b74
brunocriacoes <br.rafael@outlook.com>
2023-01-13 09:33:44 -0300

    paginas show galeria

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -7,44 +7,53 @@ import Image from 'next/image'
 export default function ProjectBySlug({ allPosts, allPostCat, allCat, slug }: any) {
 
 	let dictionary: any = {
-		'branding' : 'branding',
-		'digital-and-internet' : 'digital',
-		'work':'home',
-		'graphical-arquitecture':'graphic-architecture',
+		'branding': 'branding',
+		'digital-and-internet': 'digital',
+		'work': 'home',
+		'graphical-arquitecture': 'graphic-architecture',
 
 	}
 
 	function getName(id: any): string {
-		return allCat.find( (c:any) => c.id == id ).slug
+		return allCat.find((c: any) => c.id == id).slug
 	}
-	allPostCat = allPostCat.map( (post:any) => {
-		post.categorySlugs = post?.category?.map( (catId:number) => getName(catId) ) || []
+	allPostCat = allPostCat.map((post: any) => {
+		post.categorySlugs = post?.category?.map((catId: number) => getName(catId)) || []
 		return post
 	})
-	allPostCat = allPostCat.filter( (f:any) => f.categorySlugs.includes(dictionary?.[slug] || slug || '') )
+	allPostCat = allPostCat.filter((f: any) => f.categorySlugs.includes(dictionary?.[slug] || slug || ''))
 	return (
 		<div>
 			<HeaderComponents />
-			<div>
-				<h1 className="text-white">{allPosts[0].title}</h1>
+			<div className="container">
+				<h1 className="text-white font-hk my-8">
+					{allPosts[0].title}
+				</h1>
+
 				<div dangerouslySetInnerHTML={{ __html: allPosts[0].content }} />
-				<div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-					{allPostCat && allPostCat.map((p:any)=> <div key={p.id}>
+				<div className="columns-1 md:columns-3 gap-8 font-hk">
+					{allPostCat && allPostCat.map((p: any) => <div 
+					key={p.id}
+					className="mb-8"
+					>
 						<Link href={'project/' + p.slug} >
-							<div >
-								<div className="block relative w-full h-[500px]">
-									<Image
-										alt={p.title}
-										src={
-											p.image_full
-										}
-										layout='fill'
-										objectFit='cover'
-									/>
+							<div className="relative flex overflow-hidden">
+								<div className="block relative w-full overflow-hidden">
+									<img className=" w-full transition-all  duration-300 hover:scale-[1.05]" src={p.image_full} alt={p.title} />
 								</div>
-								<h1 className="text-white">{p.title}</h1>
-								<div dangerouslySetInnerHTML={{ __html: p.more }} />
-								{p.category && p.category.map( (id:number) => <span key={id}>#{getName(id)}</span> )}
+								<div
+									className="p-8 transition-all duration-300 opacity-0 hover:opacity-100 block absolute z-10 top-0 left-0 bg-[#C00D] w-full h-[1000px]"
+								>
+									<strong className="text-white font-bold">{p.title}</strong>
+									<div className="inline-block w-[40px] h-[1px] mb-[6px] mx-[6px] bg-[#FFF] "></div>
+									<div dangerouslySetInnerHTML={{ __html: p.more }} />
+									{p.category && p.category.map((id: number) => <span
+										key={id}
+										className="mr-2 text-[#FFF6]"
+									>
+										#{getName(id)}
+									</span>)}
+								</div>
 							</div>
 						</Link>
 					</div>)}
